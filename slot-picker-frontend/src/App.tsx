bafc9aa3156c99b7acd8fa3cc98a39bccf2d4720
Kamile Matulenaite <kamile.matu@gmail.com>
2020-11-25 21:09:18 +0000

    Add toggle for Wednesday delivery

--- slot-picker-frontend/src/App.tsx
+++ slot-picker-frontend/src/App.tsx
@@ -1,17 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import './App.css';
+import 'react-toggle/style.css';
 import { formatSlots } from './utils';
 import { FormattedSlot } from './types';
-import { add, format, isAfter, isBefore, sub } from 'date-fns';
+import { add, format, isAfter, isBefore, isWednesday, sub } from 'date-fns';
 import { SlotButton } from './components/SlotButton';
 import { BACKEND_ENDPOINT, TEXT_CONSTANTS } from './constants';
+import Toggle from 'react-toggle';
+import { PrimaryText } from './atoms/PrimaryText';
 
 const App = () => {
   const [error, setError] = useState<string | undefined>();
   const [slots, setSlots] = useState<FormattedSlot[] | undefined>();
   const [selectedSlotId, setSelectedSlotId] = useState<string | undefined>();
   const [startDateRange, setStartDateRange] = useState(sub(new Date(), { days: 1 }));
+  const [isMonstera, setIsMonstera] = useState<boolean>(false);
 
   useEffect(() => {
     axios
@@ -31,7 +35,15 @@ const App = () => {
       <header className="App-header">
         <h1>Slot Picker</h1>
         <p>Select your slot</p>
-        <div style={{ display: 'flex' }}>
+        <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+          <Toggle
+            id="monstera-status"
+            defaultChecked={isMonstera}
+            onChange={() => setIsMonstera(!isMonstera)}
+          />
+          <PrimaryText>Are we delivering a Monstera for you?</PrimaryText>
+        </div>
+        <div style={{ display: 'flex', marginTop: 20 }}>
           <button
             disabled={startDateRange <= new Date()}
             onClick={() => setStartDateRange(sub(startDateRange, { days: 4 }))}
@@ -52,33 +64,41 @@ const App = () => {
                       key={`${index}-div`}
                       style={{ display: 'flex', flexDirection: 'column', padding: 10 }}
                     >
-                      <span key={`${index}-title`} style={{ fontSize: 10 }}>
-                        {format(slot.date, 'dd MMM')}
-                      </span>
-                      <SlotButton
-                        disabled={!slot.morning.available}
-                        key={`${index}-AM`}
-                        onClick={() => setSelectedSlotId(slot.morning.id)}
-                        selected={selectedSlotId === slot.morning.id}
-                      >
-                        {slot.morning.available ? slot.morning.label : TEXT_CONSTANTS.FULL}
-                      </SlotButton>
-                      <SlotButton
-                        disabled={!slot.afternoon.available}
-                        key={`${index}-PM`}
-                        onClick={() => setSelectedSlotId(slot.afternoon.id)}
-                        selected={selectedSlotId === slot.afternoon.id}
-                      >
-                        {slot.afternoon.available ? slot.afternoon.label : TEXT_CONSTANTS.FULL}
-                      </SlotButton>
-                      <SlotButton
-                        disabled={!slot.evening.available}
-                        key={`${index}-EVE`}
-                        onClick={() => setSelectedSlotId(slot.evening.id)}
-                        selected={selectedSlotId === slot.evening.id}
-                      >
-                        {slot.evening.available ? slot.evening.label : TEXT_CONSTANTS.FULL}
-                      </SlotButton>
+                      <PrimaryText key={`${index}-title`}>
+                        {format(slot.date, 'eee dd MMM')}
+                      </PrimaryText>
+                      {isMonstera && isWednesday(slot.date) ? (
+                        <SlotButton disabled>
+                          We can't deliver monsteras on Wednesdays...
+                        </SlotButton>
+                      ) : (
+                        <>
+                          <SlotButton
+                            disabled={!slot.morning.available}
+                            key={`${index}-AM`}
+                            onClick={() => setSelectedSlotId(slot.morning.id)}
+                            selected={selectedSlotId === slot.morning.id}
+                          >
+                            {slot.morning.available ? slot.morning.label : TEXT_CONSTANTS.FULL}
+                          </SlotButton>
+                          <SlotButton
+                            disabled={!slot.afternoon.available}
+                            key={`${index}-PM`}
+                            onClick={() => setSelectedSlotId(slot.afternoon.id)}
+                            selected={selectedSlotId === slot.afternoon.id}
+                          >
+                            {slot.afternoon.available ? slot.afternoon.label : TEXT_CONSTANTS.FULL}
+                          </SlotButton>
+                          <SlotButton
+                            disabled={!slot.evening.available}
+                            key={`${index}-EVE`}
+                            onClick={() => setSelectedSlotId(slot.evening.id)}
+                            selected={selectedSlotId === slot.evening.id}
+                          >
+                            {slot.evening.available ? slot.evening.label : TEXT_CONSTANTS.FULL}
+                          </SlotButton>
+                        </>
+                      )}
                     </div>
                   );
                 })}
